50d70c235563fb4b082beaf82e48d64275ec2c1d
[Duke Gumball] <[dukegumball@gmail.com]>
2024-12-01 15:05:37 -0500

    f

--- Main Page/main.js
+++ Main Page/main.js
@@ -1,1 +1,19 @@
+// Settings //
+
+window.onload = function() {
+    
+    enableAnnouncement = true
+
+    if (enableAnnouncement == true) {
+    
+        const announcementBar = document.getElementById("MessageBoard");
+    
+        announcementBar.display = "Block"
+        
+    
+    
+    }
+}     
+
+
 
