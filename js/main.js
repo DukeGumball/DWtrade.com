937637716535bc587f1c65d7f833cab4ccd83c83
[Duke Gumball] <[dukegumball@gmail.com]>
2024-12-06 23:07:44 -0500

    f

--- js/main.js
+++ js/main.js
@@ -1,25 +1,30 @@
 // Settings //
 
-enableAnnouncement = true
+enableAnnouncement = false
 announcementTxt = "Welcome!!"
 
 // Vars //
 
-const addDoodle = document.getElementById("AddTrade")
+const addDoodle = document.getElementById("AddTrade");
+const announcementBar = document.getElementById("MessageBoard");
+const createUI = document.getElementById("DoodleCreate");
+addDoodle.onclick = function() {
+  
+  createUI.style.display = "Block"
+  
+}
 
 window.onload = function() {
-
+  
     if (enableAnnouncement == true) {
-    
-        const announcementBar = document.getElementById("MessageBoard");
-    
-        announcementBar.display = "Block"
-        announcementBar.textContent = "WELCOME!!!!"
+      
+        announcementBar.style.display = "Block"
+       
+      announcementBar.innerHTML = "WELCOME!!!!"
         
     
     }
 
 }     
-
 
 
